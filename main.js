6b6d205543e0f0204a0d24a71928739af828abc2
Guilherme Marques <71295632+GuilhermeMPCosta@users.noreply.github.com>
2023-01-25 15:08:28 -0300

    insercçao de animação

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 var vez=0
 function abre(){
     let tresbarras=document.querySelector("#select")
+    let tresbarrasvirado=document.querySelector(".tresbarras")
     if (vez==0){
         tresbarras.classList.add("open")
+        tresbarrasvirado.classList.add("tresbarrasvirado")
         console.log(vez)
         vez=1
     }else{
         tresbarras.classList.remove("open")
+        tresbarrasvirado.classList.remove("tresbarrasvirado")
         console.log(vez)
         vez=0
     } 
